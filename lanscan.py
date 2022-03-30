526cc7c261c3ac2234e8d8cb47cd01c2b36ca43b
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-03-31 03:51:03 +0900

    Update lanscan.py

--- lanscan.py
+++ lanscan.py
@@ -1,5 +1,7 @@
 import netifaces
 from scapy.all import *
+import socket
+import netaddr
 
 
 def get_own_ip():
@@ -14,14 +16,37 @@ def get_netmask():
     netmask = netifaces.ifaddresses('en0')[netifaces.AF_INET][0]['netmask']
     return netmask
 
+#ネットワークアドレス部を求める
+def get_networkaddr():
+    ip=get_own_ip()
+    ip = netaddr.IPNetwork(ip)
+
+    
+    return ip.network
+    
+
+# ipアドレスを求める関数
+
+# macアドレスを求める関数
+
+# ホスト名を取得
+def get_hostname(ip_list):
+    host_list=[]
+    for ip in ip_list:
+        try:
+            hostname = socket.gethostbyaddr(ip)[0]
+            host_list.append(hostname)
+        except:
+            host_list.append('None')
+
+    return host_list
+
 
 if __name__ == '__main__':
     own_ip=get_own_ip()
     broadcastaddr = get_broadcastaddr()
     netmask = get_netmask()
     
-    frame=IP(dst=broadcastaddr)/ICMP()
-    receive = sr(frame,timeout=1,iface='en0')
+    print(get_networkaddr())
     
-    print(receive)
-    print(broadcastaddr)
+    
