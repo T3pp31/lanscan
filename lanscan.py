96024167214015a08e356db841b8d14917704a2d
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-04-01 01:28:29 +0900

    ip,macを求めるところまで完了
    次回はnmapを使うところから再開

--- lanscan.py
+++ lanscan.py
@@ -3,6 +3,7 @@ from scapy.all import *
 import socket
 import netaddr
 import ipaddress
+import nmap
 
 def get_own_ip():
     own_ip = netifaces.ifaddresses('en0')[netifaces.AF_INET][0]['addr']
@@ -24,9 +25,43 @@ def get_networkaddr():
     network_addr = str(netaddr.IPNetwork(ip+'/'+netmask).cidr)
     
     return network_addr
-    
 
-# ipアドレスを求める関数 ネットワークアドレス-ブロードキャストアドレスまでを探したい
+#ホスト名を検索
+def get_hostname(ip):
+    try:
+        hostname = socket.gethostbyaddr(ip)[0]
+    except:
+        hostname='None'
+    return hostname
+
+# ipアドレス,macアドレス,host名,ポートを検索する
+def get_hostinformation(networkaddr):
+    ip_list=[]
+    mac_list=[]
+    host_name=[]
+    open_port=[]
+    for ip in netaddr.IPNetwork(networkaddr):
+        print(ip)
+        ip=str(ip)
+        frame = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(op=1, pdst = ip)
+        receive = srp1(frame,timeout=0.1,iface='en0')
+        try:
+            mac_list.append(receive[Ether].src)
+            ip_list.append(ip)
+            print(receive)
+        except:
+            pass
+    for ip in ip_list:
+        host_name.append(get_hostname(ip))
+        ps=nmap.PortScanner()
+        ip=str(ip)
+        open_port.append(ps.scan(ip,'1-65535','-sSV'))
+    print(f'open_port:{open_port}')
+        
+        
+        
+        
+        
 
 # macアドレスを求める関数
 
@@ -47,7 +82,15 @@ if __name__ == '__main__':
     own_ip=get_own_ip()
     broadcastaddr = get_broadcastaddr()
     netmask = get_netmask()
-
-    print(get_networkaddr())
+    network_addr = get_networkaddr()
     
+    print(f'my_ip:{own_ip}')
+    print(f'broadcast_address:{broadcastaddr}')
+    print(f'netmask:{netmask}')
+    print(f'network_address:{network_addr}')
+    
+    #get_hostinformation(network_addr)
+    ip='192.168.1.9'
+    ps=nmap.PortScanner()
+    print(ps.scan(ip,'1-65535','-sSV'))
     
