53677661e3554056eeea2a008531f3d9d59ae51c
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2023-04-10 00:27:34 +0900

    add poetry

--- lanscan.py
+++ lanscan.py
@@ -1,70 +1,77 @@
-import netifaces
-from scapy.all import *
-import socket
-import netaddr
 import ipaddress
-from tqdm import tqdm
-import socket
 import os
-import pandas as pd
+import socket
 import threading
+
+import netaddr
+import netifaces
+import pandas as pd
+from scapy.all import *
+from tqdm import tqdm
 
 
 def get_own_ip():
-    own_ip = netifaces.ifaddresses('en0')[netifaces.AF_INET][0]['addr']
+    own_ip = netifaces.ifaddresses("en0")[netifaces.AF_INET][0]["addr"]
     return own_ip
 
+
 def get_broadcastaddr():
-    broadcastaddr = netifaces.ifaddresses('en0')[netifaces.AF_INET][0]['broadcast']
+    broadcastaddr = netifaces.ifaddresses("en0")[netifaces.AF_INET][0][
+        "broadcast"
+    ]
     return broadcastaddr
 
+
 def get_netmask():
-    netmask = netifaces.ifaddresses('en0')[netifaces.AF_INET][0]['netmask']
+    netmask = netifaces.ifaddresses("en0")[netifaces.AF_INET][0]["netmask"]
     return netmask
 
-#ネットワークアドレス部を求める
+
+# ネットワークアドレス部を求める
 # ホストアドレスが出てきてしまう→ネットワークアドレスを出したい
 def get_networkaddr():
-    ip=get_own_ip()
-    netmask=get_netmask()
-    network_addr = str(netaddr.IPNetwork(ip+'/'+netmask).cidr)
-    
+    ip = get_own_ip()
+    netmask = get_netmask()
+    network_addr = str(netaddr.IPNetwork(ip + "/" + netmask).cidr)
+
     return network_addr
 
-#ホスト名を検索
+
+# ホスト名を検索
 def get_hostname(ip_list):
-    host_list=[]
+    host_list = []
     for ip in ip_list:
         try:
             hostname = socket.gethostbyaddr(ip)[0]
             host_list.append(hostname)
         except:
-            host_list.append('None')
+            host_list.append("None")
 
     return host_list
 
+
 def port_scan(ip_list):
     for ip in tqdm(ip_list):
-        for port in tqdm(range(0,65535)):
-            individual_port=[]
-            s=socket.socket()
-            errno = s.connect_ex((ip,port))
+        for port in tqdm(range(0, 65535)):
+            individual_port = []
+            s = socket.socket()
+            errno = s.connect_ex((ip, port))
             s.close()
-        
-            if errno == 0 :
+
+            if errno == 0:
                 individual_port.append(port)
         port.append(individual_port)
-        
-        
+
+
 def port_run(ip):
-    scan_range = [1, 65535];
+    scan_range = [1, 65535]
 
     host = ip
 
-    threads = [];
-    ports = [];
-    isopen = [];
-    individual_port=[]
+    threads = []
+    ports = []
+    isopen = []
+    individual_port = []
 
     def Run(port, i):
         con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -72,107 +79,85 @@ def port_run(ip):
         con.close()
 
         if return_code == 0:
-            isopen[i]=1;
-        
-    
-    count = 0;
+            isopen[i] = 1
+
+    count = 0
     for port in tqdm(range(scan_range[0], scan_range[1])):
-        ports.append(port);
-        isopen.append(0);
-        thread = threading.Thread(target=Run, args=(port, count));
-        thread.start();
-        threads.append(thread);
-        count = count + 1;
-    
-    
-        
+        ports.append(port)
+        isopen.append(0)
+        thread = threading.Thread(target=Run, args=(port, count))
+        thread.start()
+        threads.append(thread)
+        count = count + 1
+
     for i in range(len(threads)):
         threads[i].join()
         if isopen[i] == 1:
             individual_port.append(ports[i])
-            print('%d open' % ports[i])
-    
+            print("%d open" % ports[i])
+
     print(individual_port)
 
     return individual_port
 
 
-
-
-
-
-
-
-
-
-
 # ipアドレス,macアドレス,host名,ポートを検索する
 def get_hostinformation(networkaddr):
-    ip_list=[]
-    mac_list=[]
-    port=[]
+    ip_list = []
+    mac_list = []
+    port = []
 
     for ip in tqdm(netaddr.IPNetwork(networkaddr)):
         print(ip)
-        ip=str(ip)
-        frame = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(op=1, pdst = ip)
-        receive = srp1(frame,timeout=0.1,iface='en0')
+        ip = str(ip)
+        frame = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=ip)
+        receive = srp1(frame, timeout=0.1, iface="en0")
         try:
             mac_list.append(receive[Ether].src)
             ip_list.append(ip)
             print(receive)
         except:
             pass
-        
-    host=get_hostname(ip_list)
-    
+
+    host = get_hostname(ip_list)
+
     for ip in tqdm(ip_list):
-        individual_port=port_run(ip)
+        individual_port = port_run(ip)
         port.append(individual_port)
-        
-        
-        
-    
-        
-    return ip_list,mac_list,host,port
+
+    return ip_list, mac_list, host, port
 
 
-        
-        
-        
-def make_result(ip,mac,host,port):
-    df=pd.DataFrame()
-    df['ip']=ip
-    df['mac']=mac
-    df['host']=host
-    df['port']=port
-    
+def make_result(ip, mac, host, port):
+    df = pd.DataFrame()
+    df["ip"] = ip
+    df["mac"] = mac
+    df["host"] = host
+    df["port"] = port
+
     return df
 
 
-
-
-
-if __name__ == '__main__':
-    own_ip=get_own_ip()
+if __name__ == "__main__":
+    own_ip = get_own_ip()
     broadcastaddr = get_broadcastaddr()
     netmask = get_netmask()
     network_addr = get_networkaddr()
-    
-    
-    ip,mac,host,port=get_hostinformation(network_addr)
-    
 
-    #print(f'ip:{ip}')
-    #print(f'mac:{mac}')
-    #print(f'hostname:{host}')
-    #print(f'port:{port}')
-    print('-------------------------------------------------------------------------------------------')
-    
-    print(f'my_ip:{own_ip}')
-    print(f'broadcast_address:{broadcastaddr}')
-    print(f'netmask:{netmask}')
-    print(f'network_address:{network_addr}')
-    
-    df=make_result(ip,mac,host,port)
+    ip, mac, host, port = get_hostinformation(network_addr)
+
+    # print(f'ip:{ip}')
+    # print(f'mac:{mac}')
+    # print(f'hostname:{host}')
+    # print(f'port:{port}')
+    print(
+        "-------------------------------------------------------------------------------------------"
+    )
+
+    print(f"my_ip:{own_ip}")
+    print(f"broadcast_address:{broadcastaddr}")
+    print(f"netmask:{netmask}")
+    print(f"network_address:{network_addr}")
+
+    df = make_result(ip, mac, host, port)
     print(df)
